20acf50b9114e9dee343a73bfcc9b91b63ab9c33
yancey wang <yancey@yanceys-Mac-mini.local>
2023-12-29 05:17:15 -0500

    create day20 dir

---
+++ day20/main.cpp
@@ -0,0 +1,12 @@
+#include "mylibs.h"
+#include <cassert>
+#include <cstdio>
+using namespace std;
+#define mkp make_pair
+#define sz(x) ((int)x.size())
+int test = 0;
+
+int main()
+{
+    return 0;
+}
